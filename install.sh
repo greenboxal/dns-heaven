71828e74c87a4988f66401af4e3fc92c6e99673a
Jonathan Lima <greenboxal@gmail.com>
2017-09-09 21:36:18 -0300

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cat > /System/Library/LaunchDaemons/com.greenboxal.dnsheaven <<EOF
+set -e
+
+TARGET=/usr/local/bin/dns-heaven
+PLIST=/Library/LaunchDaemons/com.greenboxal.dnsheaven.plist
+
+curl -L -o $TARGET https://github.com/greenboxal/dns-heaven/releases/download/v1.0.0/dns-heaven_1.0.0_darwin_amd64
+chmod +x $TARGET
+
+cat > $PLIST <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -9,11 +17,16 @@ cat > /System/Library/LaunchDaemons/com.greenboxal.dnsheaven <<EOF
     <string>com.greenboxal.dnsheaven</string>
     <key>ProgramArguments</key>
     <array>
-        <string>/usr/local/bin/dnsheaven</string>
+        <string>$TARGET</string>
     </array>
     <key>KeepAlive</key>
+    <true/>
+    <key>RunAtLoad</key>
     <true/>
 </dict>
 </plist>
 EOF
+
+chmod 644 $PLIST
+launchctl load -w $PLIST
 
